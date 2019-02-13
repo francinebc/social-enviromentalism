ffb7e9a6371d263a3bdba49769c99de6881cd888
Francinebc <francinebc.mail@gmail.com>
2019-02-14 10:08:25 +1300

    Semantic ui for nav bar

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
 
 export default class Nav extends React.Component {
-    
-    constructor () {
-        super()
-    }
+  constructor() {
+    super()
+  }
 
-    render () {
-        return (
-            <div>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/challenges'>Challenges</Link></li>
-                    <li><Link to='/sign-up'>sign up</Link>/<Link to='/login'>login</Link></li>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Menu>
+          <Menu.Item>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/challenges">Challenges</Link>
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Link to="/sign-up">sign up</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/login">login</Link>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </div>
+    )
+  }
 }
