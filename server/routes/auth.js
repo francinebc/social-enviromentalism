ac78e8e591fb18ad08db760f6cce741927ac23a8
francinebc <francinebc.mail@gmail.com>
2019-02-19 16:48:01 +1300

    Beginning auth process

---
+++ server/routes/auth.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express();
+const db = require("../../db");
+
+router.post("/register", register);
+
+function register(req, res) {
+  db.createUser(req.body)
+    .then(([id]) => {
+      res.locals.userId = id;
+    })
+    .then(() => res.status(201).json({ ok: true }))
+    .catch(({ message }) => {
+      // This is vulnerable to changing databases. SQLite happens to use
+      // this message, but Postgres doesn't.
+      if (message.includes("UNIQUE constraint failed: users.username")) {
+        return res.status(400).json({
+          ok: false,
+          message: "Username already exists."
+        });
+      }
+      res.status(500).json({
+        ok: false,
+        message: "Something bad happened. We don't know why."
+      });
+    });
+}
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
+
 const path = require('path')
 const express = require('express')
 const dbRoutes = require('./routes/db')
 const redditRoutes = require('./routes/reddit')
+const authRoutes = require('./routes/auth')
 
 const server = express()
 
@@ -9,5 +12,6 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 server.use('/api/v1', dbRoutes)
 server.use('/api/v1', redditRoutes)
+server.use('/api/v1/auth', authRoutes)
 
 module.exports = server
