ef967a29797a752abb9808cdf845639a2798ba73
francinebc <francinebc.mail@gmail.com>
2019-03-22 16:41:48 +1300

    registration now working

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,29 +1,82 @@
-const express = require("express");
-const router = express();
-const db = require("../db/db");
+const express = require('express')
+const db = require('../db/users')
+const router = express.Router()
+const token = require('../auth/token')
+const hash = require('../auth/hash')
 
-router.post("/register", register);
+router.post('/register', validateRegister, register, token.issue)
+router.post('/login', validateLogin, checkUser, token.issue)
 
-function register(req, res) {
-  db.createUser(req.body)
-    .then(([id]) => {
-      res.locals.userId = id;
+function validateRegister (req, res, next) {
+  const {email, password, firstName, lastName} = req.body
+  if (!firstName) {
+    return next(new Error('No first name provided'))
+  }
+  if (!lastName) {
+    return next(new Error('No last name provided'))
+  }
+  if (!email) {
+    return next(new Error('No email provided'))
+  }
+  if (!password) {
+    return next(new Error('No password provided'))
+  }
+
+  next()
+} 
+
+function register (req, res, next) {
+  db.registerUser(req.body)
+    .then((user) => {
+      res.locals.userId = user[0]
+      next()
     })
-    .then(() => res.status(201).json({ ok: true }))
-    .catch(({ message }) => {
-      // This is vulnerable to changing databases. SQLite happens to use
-      // this message, but Postgres doesn't.
-      if (message.includes("UNIQUE constraint failed: users.username")) {
-        return res.status(400).json({
-          ok: false,
-          message: "Username already exists."
-        });
-      }
-      res.status(500).json({
-        ok: false,
-        message: "Something bad happened. We don't know why."
-      });
-    });
+    .catch(({message}) => {
+      message.includes('UNIQUE constraint failed: users.username')
+        ? registrationError(res, 'User already exists.', 400)
+        : registrationError(res, `Something bad happened. We don't know why.`, 500)
+    })
 }
 
-module.exports = router;
+function validateLogin (req, res, next) {
+  const {email, password} = req.body
+  if (!email) {
+    return next(new Error('No username provided'))
+  }
+  if (!password) {
+    return next(new Error('No password provided'))
+  }
+
+  next()
+}
+
+function checkUser (req, res, next) {
+  db.getUser(req.body)
+    .then(user => {
+      if (user) res.locals.userId = user.id
+      return user && hash.verify(user.hash, req.body.password)
+    })
+    .then(isValid => {
+      return isValid ? next() : invalidCredentials(res)
+    })
+    .catch(() => {
+      res.status(400).json({
+        errorType: 'DATABASE_ERROR'
+      })
+    })
+}
+
+function invalidCredentials (res) {
+  res.status(400).json({
+    errorType: 'INVALID_CREDENTIALS'
+  })
+}
+
+function registrationError (res, errorMessage, errorCode) {
+  res.status(errorCode).json({
+    ok: false,
+    message: errorMessage
+  })
+}
+
+module.exports = router
